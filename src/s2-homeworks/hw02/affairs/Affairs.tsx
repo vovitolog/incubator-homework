110324e105ccc7ac19c4e342b0ff7e9a59903800
vovitolog <vovitolog@gmail.com>
2022-11-20 22:49:11 +0300

    feat: add homework 2 solution

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,24 +4,28 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[] // need to fix any
+    setFilter: (filter: FilterType) => void;
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
     const setAll = () => {
-        // need to fix
+       props.setFilter('all');
+       console.log(props.filter)
     }
     const setHigh = () => {
-        // need to fix
+        props.setFilter('high');
+        console.log(props.filter)
     }
     const setMiddle = () => {
-        // need to fix
+        props.setFilter('middle');
+        console.log(props.filter)
     }
     const setLow = () => {
-        // need to fix
+        props.setFilter('low');
+        console.log(props.filter)
     }
 
     const cnAll = s.button + ' ' + s.all + (props.filter === 'all' ? ' ' + s.active : '')
