9c292f8c9c6340f1108e891c534609c18c362cd3
vovitolog <vovitolog@gmail.com>
2023-01-24 21:59:30 +0300

    feat: add Homework 10 solution

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,20 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch = useDispatch();
+    const selector = useSelector((state: AppStoreType)=> state.loading)
+    const isLoading = selector.isLoading
+    console.log(isLoading)
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+   // const isLoading = false
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true));
         // setTimeout
+        setTimeout(()=> {
+            dispatch(loadingAC(false))
+        },1500)
     }
 
     return (
